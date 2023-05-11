223de80d600ca9f943c67bbcf11b68c8ebc9c1bc
Alex Jercan <jercan_alex27@yahoo.com>
2023-05-11 20:27:53 +0300

    feat: clap deez nuts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+/// Simple program to munge a wordlist
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the wordlist to munge
+    #[arg(short, long)]
+    wordlist: Option<String>,
+
+    /// Path to the output file
+    #[arg(short, long)]
+    output: Option<String>,
+
+    /// Munge level
+    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(1..9))]
+    level: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
+
 }
