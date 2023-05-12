e876b27bf2b695fe0c9db90ba629d779045323a7
Alex Jercan <jercan_alex27@yahoo.com>
2023-05-12 21:04:58 +0300

    feat: munger be mungin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fs::File,
+    hash::Hash,
     io::{self, BufRead, BufReader, BufWriter, Write},
 };
 
@@ -42,43 +43,128 @@ fn write_words(words: &Vec<String>, file: &Option<String>) -> Result<(), io::Err
     return stream.flush();
 }
 
-fn leet_speak(word: &str) -> Vec<String> {
-    // TODO: with combinations
-    return vec![
-        word.to_string(),
-        word.replace("e", "3"),
-        word.replace("a", "4"),
-        word.replace("o", "0"),
-        word.replace("i", "!"),
-        word.replace("i", "1"),
-        word.replace("l", "1"),
-        word.replace("a", "@"),
-        word.replace("s", "$"),
-        word.replace("s", "5"),
-        word.replace("t", "7"),
-    ];
+fn generate_subsequences<K, V>(mappings: &Vec<(K, Vec<V>)>, index: usize) -> Vec<HashMap<K, V>>
+where
+    K: Eq + Hash + Clone,
+    V: Clone,
+{
+    let mut subsequences = vec![];
+
+    if index == mappings.len() {
+        subsequences.push(HashMap::new());
+    } else {
+        let (key, values) = &mappings[index];
+
+        let subsubsequences = generate_subsequences(mappings, index + 1);
+
+        for value in values {
+            for subsubsequence in &subsubsequences {
+                let mut subsequence = subsubsequence.clone();
+                subsequence.insert(key.clone(), value.clone());
+                subsequences.push(subsequence);
+            }
+        }
+
+        subsequences.extend(subsubsequences);
+    }
+
+    return subsequences;
 }
 
-fn word_capitalization(word: &str) -> Vec<String> {
-    return vec![
-        word.to_string(),
-        word.to_lowercase(),
-        word.to_uppercase(),
-        word[0..1].to_uppercase() + &word[1..],
-    ];
+trait MungeBlock {
+    fn munge(&self, word: &str) -> Vec<String>;
 }
 
-fn munge_word(word: &str) -> Vec<String> {
-    let mut words = vec![word.to_string()];
+struct LeetSpeak<'a> {
+    mappings: Vec<HashMap<&'a str, &'a str>>,
+}
 
-    words.extend(leet_speak(word));
-    words.extend(word_capitalization(word));
-
-    return words
-        .drain(..)
-        .collect::<HashSet<_>>()
+impl<'a> LeetSpeak<'a> {
+    fn new() -> Self {
+        let mappings: Vec<(_, Vec<_>)> = vec![
+            ("e", "3"),
+            ("a", "4"),
+            ("a", "@"),
+            ("o", "0"),
+            ("i", "1"),
+            ("i", "!"),
+            ("l", "1"),
+            ("s", "5"),
+            ("s", "$"),
+        ]
         .into_iter()
-        .collect::<Vec<_>>();
+        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, (k, v)| {
+            acc.entry(k).and_modify(|e| e.push(v)).or_insert(vec![k, v]);
+            return acc;
+        })
+        .into_iter()
+        .collect();
+
+        let mappings = generate_subsequences(&mappings, 0);
+
+        return LeetSpeak { mappings };
+    }
+}
+
+impl<'a> MungeBlock for LeetSpeak<'a> {
+    fn munge(&self, word: &str) -> Vec<String> {
+        return self
+            .mappings
+            .iter()
+            .map(|mapping| {
+                mapping
+                    .iter()
+                    .fold(word.to_owned(), |acc, (k, v)| acc.replace(k, v))
+            })
+            .collect();
+    }
+}
+
+struct Capitalization;
+
+impl Capitalization {
+    fn new() -> Self {
+        return Capitalization {};
+    }
+}
+
+impl MungeBlock for Capitalization {
+    fn munge(&self, word: &str) -> Vec<String> {
+        return vec![
+            word.to_string(),
+            word.to_lowercase(),
+            word.to_uppercase(),
+            word[0..1].to_uppercase() + &word[1..],
+        ];
+    }
+}
+
+struct Munger {
+    blocks: Vec<Box<dyn MungeBlock>>,
+}
+
+impl MungeBlock for Munger {
+    fn munge(&self, word: &str) -> Vec<String> {
+        let mut words = vec![word.to_string()];
+
+        for block in &self.blocks {
+            words.extend(block.munge(word));
+        }
+
+        return words
+            .drain(..)
+            .collect::<HashSet<_>>()
+            .into_iter()
+            .collect::<Vec<_>>();
+    }
+}
+
+macro_rules! munger {
+    ( $( $x:expr ),* ) => {
+        Munger {
+            blocks: vec![ $( Box::new($x) ),* ],
+        }
+    };
 }
 
 fn main() {
@@ -86,7 +172,9 @@ fn main() {
 
     let words = read_words(&args.wordlist).unwrap();
 
-    let words = words.iter().flat_map(|w| munge_word(w)).collect();
+    let munger = munger![LeetSpeak::new(), Capitalization::new()];
+
+    let words = words.iter().flat_map(|w| munger.munge(w)).collect();
 
     write_words(&words, &args.output).unwrap();
 }
